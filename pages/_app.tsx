609d47181ee8de1a33fbd13bfb903c338c710750
Kluskey <jared.daniel.mccluskey@gmail.com>
2021-10-08 11:55:11 -0400

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Script>
+      var a
+      var rc = new RegExp('_bn_d=([^;]+)')
+      var rq = new RegExp('_bn_d=([^&#]*)', 'i')
+      var aq = rq.exec(window.location.href)
+      if (aq != null) a = aq
+      else var ac = rc.exec(document.cookie)
+      if (ac != null) a = ac
+      if (a != null) {
+  var _bn_d = a[1]
+      ;(function () {
+    var pl = document.createElement('script')
+      pl.type = 'text/javascript'
+      pl.async = true
+      pl.src =
+      ('https:' == document.location.protocol ? 'https://app' : 'http://app') +
+      '.postaladmin.com/plDataEmbed.js'
+      var s = document.getElementsByTagName('script')[0]
+      s.parentNode.insertBefore(pl, s)
+  })()
+}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 export default MyApp
